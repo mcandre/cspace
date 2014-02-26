7043e18a95f9e83955f8244ce92b1c157d071696
Andrew Pennebaker <apennebaker@42six.com>
2014-02-26 12:00:10 -0500

    fewer imports

--- treetograph.hs
+++ treetograph.hs
@@ -6,8 +6,7 @@ import Data.Text.Lazy (Text, pack)
 import Data.List (nub)
 import Data.Tree
 import Data.Graph.Inductive
-import Control.Parallel.Strategies
-import Control.Parallel
+import Control.Parallel.Strategies (parMap, rseq)
 
 getEdges :: (Ord t) => Tree t -> [(t, t, Text)]
 getEdges (Node x xs) = nub $ parMap rseq (\y -> (x, rootLabel y, pack "")) xs ++ concatMap getEdges xs
